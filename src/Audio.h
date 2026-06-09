dad19a1187bab8f578018437cd01bfeb449087b0
Jacek <43666290+hevet@users.noreply.github.com>
2026-06-09 09:37:49 +0200

    Update Audio.h

--- src/Audio.h
+++ src/Audio.h
@@ -365,9 +365,9 @@ class Audio {
         uint16_t DMA_DESC_NUM = 32;        // number of I2S DMA buffer
         uint16_t DMA_FRAME_NUM = 256;      // number of frames in one DMA buffer
         uint16_t FREQ_LS_HZ = 500;         // IIR Filter, lowshelf
-        uint16_t FREQ_PEAK_HZ = 3000;      // IIR Filter, peakingEQ
+        uint16_t FREQ_PEAK_HZ = 1800;      // IIR Filter, peakingEQ
         uint16_t FREQ_HS_HZ = 6000;        // IIR Filter, highshelf
-        float    QUALITY_SLOPE = 1.000;    // Quality (all shelfes)
+        float    QUALITY_SLOPE = 0.707;    // Quality (all shelfes)
         uint16_t PEAK_HOLD_SAMPLES = 2000; // VU_meter, (2000) ca. 20 ms @ 48 kHz
         uint8_t  PEAK_RELEASE = 1;         // VU_meter, Fall rate
         bool     VU_LEVEL = true;          // true: vu meter is enabled
